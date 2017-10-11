ad6bbca088a7d32d0ea9dde63437a0826e5583eb
RevelsGit <revels0k@yandex.ru>
2017-10-11 14:40:58 +0300

    ramda's lenses are now working inside of middleware!

--- src/commands/middleware/checkAttachment.js
+++ src/commands/middleware/checkAttachment.js
@@ -1,7 +1,11 @@
+import R from 'ramda';
+
 export default () => async (next, context) => {
   if (context.message.attachments.size) {
     const url = context.message.attachments.first().url;
-    Object.assign(context, { args: Object.assign(context.args, { url }) });
+    // TODO: refactor
+    const newContext = R.set(R.lensPath(['args', 'url']), url, context);
+    return next(newContext);
   }
 
   return next(context);
