8c69dabd12c113fe72f30eb3a01d8482b837881d
RevelsGit <revels0k@yandex.ru>
2017-10-13 12:34:45 +0300

    refactored injectUser middleware

--- src/commands/middleware/injectUser.js
+++ src/commands/middleware/injectUser.js
@@ -3,15 +3,11 @@ import { ErrorResponse } from '../responses';
 
 export default () => async (next, context) => {
   const { message: { author: { id: discordId } } } = context;
-  let user = await User.findOneByDiscordId(discordId);
-
-  if (user) return next({ ...context, user });
-
   try {
-    user = await new User({ discordId }).save();
+    let user = await User.findOneByDiscordId(discordId);
+    if (!user) user = await new User({ discordId }).save();
+    return next({ ...context, user });
   } catch (err) {
     return ErrorResponse(err.message, context);
   }
-
-  return next({ ...context, user });
 };
