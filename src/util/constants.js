783fc22bf50c5ee3205d90c6df700f5675188016
Dmytro Artamonov <revels0k@gmail.com>
2019-03-23 17:46:15 +0200

    Subscription model

--- src/util/constants.js
+++ src/util/constants.js
@@ -31,7 +31,8 @@ export const COMMAND_TRIGGERS = {
   PAUSE: ['pause'],
   RESUME: ['resume'],
   EH: ['eh'],
-  BSSUBSCRIBE: ['bssubscribe'],
+  MAPPER_SUB: ['mappersub'],
+  MAPPER_UNSUB: ['mapperunsub'],
 };
 
 export const FONTS = {
@@ -81,3 +82,7 @@ export const BOT_PREFIXES = [
   '=',
   '?',
 ];
+
+export const Topics = {
+  MAPPER: 1,
+};
