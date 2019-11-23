a49fd832188ca3ba2306423901bf32d05e92ae96
Dmytro Artamonov <revels0k@gmail.com>
2019-11-23 08:14:13 +0200

    removed subscription model

--- src/commands/middleware/index.ts
+++ src/commands/middleware/index.ts
@@ -23,6 +23,4 @@ export { default as stopCurrentlyPlaying } from './stopCurrentlyPlaying';
 export { default as ensureIsNotPaused } from './ensureIsNotPaused';
 export { default as ensureIsPaused } from './ensureIsPaused';
 export { default as withCooldown } from './withCooldown';
-export { default as checkSubscribedToMapper } from './checkSubscribedToMapper';
-export { default as ensureSubscribedToMapper } from './ensureSubscribedToMapper';
 export { default as injectContext } from './injectContext';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,2 @@
 export { default as User } from './user';
 export { default as Guild } from './guild';
-export { default as Subscription } from './subscription';
