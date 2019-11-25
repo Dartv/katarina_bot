f7d77e0a20eb9799dfaf8d44f539b0513f3b2111
Dmytro Artamonov <revels0k@gmail.com>
2019-11-25 17:04:54 +0200

    increased 5 star rate

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -15,7 +15,7 @@ import { injectUser } from './middleware';
 const unlink = promisify(fs.unlink);
 
 const getStarRating = (popularity: number): CharacterStar => {
-  if (popularity <= 300) return CharacterStar.FIVE_STAR;
+  if (popularity <= 500) return CharacterStar.FIVE_STAR;
   if (popularity <= 3000) return CharacterStar.FOUR_STAR;
   if (popularity <= 10000) return CharacterStar.THREE_STAR;
   return CharacterStar.TWO_STAR;
