453a1292c83e49c402ae35d9321e2e7667d10f73
Dmytro Artamonov <revels0k@gmail.com>
2019-12-13 20:29:07 +0200

    disallow steal if user does not have at least one five star character

--- src/commands/stealwaifu.ts
+++ src/commands/stealwaifu.ts
@@ -4,7 +4,7 @@ import { times } from 'ramda';
 import { ObjectId } from 'mongodb';
 
 import { ICommand } from '../types';
-import { COMMAND_TRIGGERS, Emoji } from '../util';
+import { COMMAND_TRIGGERS, Emoji, CharacterStar } from '../util';
 import { injectUser } from './middleware';
 import { Character, User } from '../models';
 import { ErrorResponse } from './responses';
@@ -26,14 +26,14 @@ const removeRandomCharacter = (
   return id;
 };
 
-const getStolenCharacters = async (...ids: ObjectId[]): Promise<[ICharacter, ICharacter[]]> => {
+const getStolenCharacters = async (...ids: ObjectId[]): Promise<ICharacter[]> => {
   const query = {
     _id: { $in: ids },
   };
   const characters = await Character.find(query).populate('series');
   return [
     characters.find(({ _id }) => _id.equals(ids[0])),
-    characters.filter(({ _id }) => !_id.equals(ids[0])),
+    ...characters.filter(({ _id }) => !_id.equals(ids[0])),
   ];
 };
 
@@ -41,22 +41,29 @@ const handler = async (context) => {
   try {
     const { user, message }: { message: Message } & Partial<any> = context;
 
-    const mentionedMember = await message.mentions.members.first();
+    const mentionedMember = message.mentions.members.first();
 
     if (!mentionedMember) return null;
-
-    const mentionedUser: any = await User.findOne({ discordId: mentionedMember.id });
-
-    if (!mentionedUser) {
-      return ErrorResponse(`${mentionedMember.displayName} doesn't have a local profile`, context);
-    }
 
     if (user.characters.length < ROUND_TRIPS) {
       return ErrorResponse('You don\'t have enough waifus', context);
     }
 
+    const [mentionedUser, fiveStarChar]: any = await Promise.all([
+      User.findOne({ discordId: mentionedMember.id }),
+      Character.findOne({ _id: { $in: user.characters }, stars: CharacterStar.FIVE_STAR }),
+    ]);
+
+    if (!mentionedUser) {
+      return ErrorResponse(`${mentionedMember.displayName} doesn't have a local profile`, context);
+    }
+
     if (!mentionedUser.characters.length) {
       return ErrorResponse(`${mentionedMember.displayName} doesn't have enough waifus`, context);
+    }
+
+    if (!fiveStarChar) {
+      return ErrorResponse('You should have at least one five star character', context);
     }
 
     const theirCharId = removeRandomCharacter(mentionedUser);
@@ -71,7 +78,7 @@ const handler = async (context) => {
 
     const [
       stolenCharacter,
-      lostCharacters,
+      ...lostCharacters
     ] = await getStolenCharacters(theirCharId, ...myCharIds);
     const lostText = lostCharacters.map(({ name, stars }) => `${name} ${Emoji.STAR.repeat(stars)}`).join(', ');
 
