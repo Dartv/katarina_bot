5921209ac2a4e7fff4df8ec5e6cf54ce7a17591a
Dmytro Artamonov <revels0k@gmail.com>
2020-01-25 22:27:48 +0200

    fixed level up notification

--- src/models/characterInfo/index.ts
+++ src/models/characterInfo/index.ts
@@ -39,14 +39,16 @@ CharacterInfoSchema.index({ user: 1, character: 1 }, { unique: true });
 CharacterInfoSchema.pre<ICharacterInfo>('save', async function () {
   if (this.exp >= EXPToLVLUp[this.level + 1]) {
     this.level += 1;
-    const character = await Character.findOne({ _id: this.character }).populate('series');
-    if (character) {
-      const embed = createCharacterEmbed({
-        ...character,
-        level: this.level,
-        exp: this.exp,
-      });
-      this.$locals.context.message.reply(`${character.name} leveled up`, { embed });
+    if (this.$locals.context) {
+      const character = await Character.findOne({ _id: this.character }).populate('series').lean();
+      if (character) {
+        const embed = createCharacterEmbed({
+          ...character,
+          level: this.level,
+          exp: this.exp,
+        });
+        this.$locals.context.message.reply(`${character.name} leveled up`, { embed });
+      }
     }
   }
 });
