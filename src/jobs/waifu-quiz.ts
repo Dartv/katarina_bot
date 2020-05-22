1c01849fd2d5b7e69f88bbfe8aedfb9d8effd2db
Dmytro Artamonov <revels0k@gmail.com>
2020-05-23 00:27:23 +0300

    delete all previous messages in a quiz

--- src/jobs/waifu-quiz.ts
+++ src/jobs/waifu-quiz.ts
@@ -15,9 +15,7 @@ export default (agenda: Agenda, client: Client) => {
       const promises = client.channels
         .filter((channel: TextChannel) => channel.name === CHANNEL_NAME)
         .map(async (channel: TextChannel) => {
-          const prevMessages = await channel
-            .fetchMessages({ limit: 100 })
-            .then((messages) => messages.filter((message) => message.author.bot));
+          const prevMessages = await channel.fetchMessages({ limit: 100 });
           const [message]: [Message, any] = await Promise.all([
             handler({
               dispatch: channel.send.bind(channel),
