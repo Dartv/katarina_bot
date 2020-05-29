5336c5ed8559626bb3036d89e1d0f76b7907498d
Dmytro Artamonov <revels0k@gmail.com>
2020-05-29 09:15:41 +0300

    fixed awakening

--- src/models/character/methods/awaken.ts
+++ src/models/character/methods/awaken.ts
@@ -7,7 +7,7 @@ const restrictStars = R.clamp(2, 6);
 
 const awaken: ICharacter['awaken'] = function (user) {
   const copies = user.characters.filter(id => this._id.equals(id));
-  const additionalStars = getCharacterAdditionalStars(this.stars, copies.length);
+  const additionalStars = getCharacterAdditionalStars(copies.length);
 
   this.stars = restrictStars(this.stars + additionalStars);
   this.$locals.additionalStars = additionalStars;
