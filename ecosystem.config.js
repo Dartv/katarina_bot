0e4a5a3be8823a6098d8515d59b90c684db02115
Dmytro Artamonov <revels0k@gmail.com>
2020-06-16 00:34:04 +0300

    updated ip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
     production: {
       key: '~/.ssh/katarina.pem',
       user: 'ubuntu',
-      host: '52.59.224.212',
+      host: '35.156.209.29',
       ref: 'origin/master',
       repo: 'git@github.com:Dartv/katarina_bot.git',
       path: '/opt/pm2/katarina',
