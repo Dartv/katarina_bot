d86ea4134b5bd67c715358bd51e62a0123a52189
Dmytro Artamonov <revels0k@gmail.com>
2020-07-18 00:55:13 +0300

    balance command

---
+++ src/commands/gacha/BalanceCommand.ts
@@ -0,0 +1,25 @@
+import { Command } from 'diskat';
+
+import { Context, UserDocument } from '../../types';
+import { Trigger } from '../../utils/constants';
+import { injectUser } from '../middleware';
+
+interface BalanceCommandContext extends Context {
+  user: UserDocument;
+}
+
+const BalanceCommand: Command<BalanceCommandContext> = async (context) => {
+  const { message, user } = context;
+
+  return message.reply(`You have ${user.currency} 💎`);
+};
+
+BalanceCommand.config = {
+  triggers: Trigger.BALANCE,
+  middleware: [
+    injectUser(),
+  ],
+  description: 'View your balance',
+};
+
+export default BalanceCommand;
--- src/commands/gacha/index.ts
+++ src/commands/gacha/index.ts
@@ -1,3 +1,4 @@
 export { default as RollCommand } from './RollCommand';
 export { default as SearchCommand } from './SearchCommand';
 export { default as BannerCommand } from './BannerCommand';
+export { default as BalanceCommand } from './BalanceCommand';
