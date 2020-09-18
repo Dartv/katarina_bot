38708a856befedfc9e00d850d1fcf9974b7504b9
Dmytro Artamonov <revels0k@gmail.com>
2020-09-18 17:47:14 +0300

    disable leaderboard monitor for now

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -3,4 +3,4 @@ export { BattleRoyaleQueueJob } from './BattleRoyaleQueueJob';
 export { BattleRoyaleJob } from './BattleRoyaleJob';
 export { ResetMissionsJob } from './ResetMissionsJob';
 export { ScoreMonitorJob } from './ScoreMonitorJob';
-export { ScoresaberLeaderboardMonitorJob } from './ScoresaberLeaderboardMonitorJob';
+// export { ScoresaberLeaderboardMonitorJob } from './ScoresaberLeaderboardMonitorJob';
