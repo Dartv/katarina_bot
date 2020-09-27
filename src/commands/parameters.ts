21eb69d9b334dea4ba9b8accb18f5195746760c4
Dmytro Artamonov <revels0k@gmail.com>
2020-09-27 15:46:56 +0300

    updated parameters to reflect lib changes

--- src/commands/parameters.ts
+++ src/commands/parameters.ts
@@ -1,16 +1,22 @@
-import { TypeResolverFunction, TypeResolver, ParameterType } from 'diskat';
+import {
+  TypeResolverFunction,
+  TypeResolver,
+  ParameterType,
+  TypeResolverContext,
+} from 'diskat';
 import { User as DiscordUser, GuildMember } from 'discord.js';
 
 import { LocalParameterType } from '../utils/constants';
 import { User } from '../models';
+import { UserDocument } from '../types';
 
 export const Parameters: Record<LocalParameterType, TypeResolverFunction> = {
-  [LocalParameterType.DB_USER]: TypeResolver.compose(
+  [LocalParameterType.DB_USER]: TypeResolver.compose<TypeResolverContext<DiscordUser>, UserDocument>(
     ParameterType.USER,
-    (user: DiscordUser) => User.findOne({ discordId: user.id }),
+    async ({ value: user }) => User.findOne({ discordId: user.id }),
   ),
-  [LocalParameterType.DB_MEMBER]: TypeResolver.compose(
+  [LocalParameterType.DB_MEMBER]: TypeResolver.compose<TypeResolverContext<GuildMember>, UserDocument>(
     ParameterType.MEMBER,
-    (user: GuildMember) => User.findOne({ discordId: user.id }),
+    async ({ value: user }) => User.findOne({ discordId: user.id }),
   ),
 };
