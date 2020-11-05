6e3b5ab6bc400d03edfe09f08b754705931e8f7f
Dmytro Artamonov <revels0k@gmail.com>
2020-11-05 21:24:04 +0200

    fixed a bug where mission completion embed would show in waifu wars channel

--- src/plugins/VersusPlugin.ts
+++ src/plugins/VersusPlugin.ts
@@ -1,32 +1,37 @@
 import { User as DiscordUser } from 'discord.js';
 
-import { Plugin } from '../types';
+import { MissionEventContext, Plugin } from '../types';
 import { MissionCode, Trigger } from '../utils/constants';
 import { Guild, User } from '../models';
 
 export const VersusPlugin: Plugin = (client) => {
   client.on('messageReactionAdd', async (reaction, author: DiscordUser) => {
-    const { message } = reaction;
-    if (message.guild && reaction.emoji.name === '❤️') {
-      const guild = await Guild.findOne({ _id: message.guild.id });
+    try {
+      const { message } = reaction;
+      if (message.guild && reaction.emoji.name === '❤️') {
+        const [guild, user] = await Promise.all([
+          Guild.findOne({ discordId: message.guild.id }),
+          User.register(author),
+        ]);
 
-      if (guild && guild.settings?.warsChannel === message.channel.id) {
-        const user = await User.findOne({ discordId: author.id });
-        if (user) {
+        if (guild && user && guild.settings.warsChannel === message.channel.id) {
           message.author = author;
 
-          const context = {
+          const context: MissionEventContext = {
             ...client.dispatcher.createContext({
               command: client.commands.get(Trigger.VERSUS[0]),
               message,
               args: {},
             }),
             user,
+            silent: true,
           };
 
           client.emitter.emit('mission', MissionCode.VERSUS_DAILY, null, context);
         }
       }
+    } catch (err) {
+      client.logger.error(err);
     }
   });
 };
