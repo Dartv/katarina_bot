99e11506ae475fe4bc69d7f7176e6c1e0fddd15d
D A <dmytro.artamonov@gmail.com>
2023-02-06 09:11:55 +0200

    banner 2w -> 1w

--- src/models/Banner/methods.ts
+++ src/models/Banner/methods.ts
@@ -8,7 +8,7 @@ import {
 import { BannerDocument } from '../../types';
 
 export const isExpired: BannerDocument['isExpired'] = function () {
-  const twoWeeksAgo = subWeeks(startOfDay(new Date()), 2);
+  const twoWeeksAgo = subWeeks(startOfDay(new Date()), 1);
   const createdAt = startOfDay(this.createdAt);
   return !!this.endedAt || isEqual(twoWeeksAgo, createdAt) || isAfter(twoWeeksAgo, this.createdAt);
 };
